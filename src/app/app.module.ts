9beb8239857e50d6a91c05654a986ae22d129688
Eder Catini <edergcatini@icloud.com>
2019-11-18 19:41:07 -0300

    Provide Calculator Service | Calculator Layout/Structure (Bootstrap)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorModule } from './calculator';
-import { TestModule } from './test';
-import { DummyModule } from './dummy';
 
 @NgModule({
   declarations: [
@@ -12,9 +10,7 @@ import { DummyModule } from './dummy';
   ],
   imports: [
     BrowserModule,
-    CalculatorModule,
-    TestModule,
-    DummyModule
+    CalculatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
