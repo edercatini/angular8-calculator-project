fe5525798b8e7515aa95b3916c7efba6a447e58d
Eder Catini <edergcatini@icloud.com>
2019-11-18 23:04:27 -0300

    Provide service features and its following unit tests

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
@@ -6,7 +6,38 @@ describe('CalculatorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: CalculatorService = TestBed.get(CalculatorService);
-    expect(service).toBeTruthy();
+    expect(TestBed.get(CalculatorService)).toBeTruthy();
   });
+
+  it('must sum two numbers', inject([CalculatorService], (service: CalculatorService) => {
+      expect(service.process(10, 20, CalculatorService.SUM)).toEqual(30);
+    })
+  );
+
+  it('must subtract two numbers', inject([CalculatorService], (service: CalculatorService) => {
+      expect(service.process(15, 10, CalculatorService.MINUS)).toEqual(5);
+    })
+  );
+
+  it('must multiplicate two numbers', inject([CalculatorService], (service: CalculatorService) => {
+      expect(service.process(10, 10, CalculatorService.TIMES)).toEqual(100);
+    })
+  );
+
+  it('must divide two numbers', inject([CalculatorService], (service: CalculatorService) => {
+      expect(service.process(10, 5, CalculatorService.DIVISION)).toEqual(2);
+    })
+  );
+
+  it('must return zero when setting invalid math operators', inject([CalculatorService], (service: CalculatorService) => {
+      expect(service.process(10, 10, '%')).toEqual(0);
+    })
+  );
+
+  it('must process only once', inject([CalculatorService], (service: CalculatorService) => {
+      spyOn(service, 'process');
+      service.process(10, 20, '+');
+      expect(service.process).toHaveBeenCalledTimes(1);
+    })
+  );
 });
--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -5,5 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
+  static readonly SUM: string = '+';
+
+  static readonly MINUS: string = '-';
+
+  static readonly TIMES: string = '*';
+
+  static readonly DIVISION: string = '/';
+
   constructor() { }
+
+  process(firstNumber: number, secondNumber: number, mathOperator: string): number {
+    let result: number;
+
+    switch (mathOperator) {
+      case CalculatorService.SUM:
+        result = firstNumber + secondNumber;
+        break;
+      case CalculatorService.MINUS:
+        result = firstNumber - secondNumber;
+        break;
+      case CalculatorService.TIMES:
+        result = firstNumber * secondNumber;
+        break;
+      case CalculatorService.DIVISION:
+        result = firstNumber / secondNumber;
+        break;
+      default:
+        result = 0;
+    }
+
+    return result;
+  }
 }
